d0cccd53fcb7845077af9c059abe9828f441762c
agushmda <ahermida97@gmail.com>
2021-09-20 21:33:07 -0300

    s

--- m04/cl11/mercado-liebre/app.js
+++ m04/cl11/mercado-liebre/app.js
@@ -12,5 +12,9 @@ app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
 })
 
+app.get('/register', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+})
+
 // Levantar el puerto
 app.listen(3000, () => console.log('Server corriendo en el puerto 3000'));
