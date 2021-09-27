1a1370a532f5de7aaa535629782aab5d0172dbe8
agushmda <ahermida97@gmail.com>
2021-09-27 12:22:03 -0300

    x

--- m04/cl11/mercado-liebre/app.js
+++ m04/cl11/mercado-liebre/app.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Config public
-app.use(express.static(path.resolve(__dirname, 'public')));
+/* Config express */
+app.use(express.static('public'));
 
 
-// Routes
-app.get('/', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'views/home.html'));
-})
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+/* Register */
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.post('/register', (req,res)=> {
+    res.send('Ya te registraste campeón');
+});
+/* Login */
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.post('/login', (req,res)=>{
+    res.send('Ya te logueaste campeón');
+});
 
-app.get('/register', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'views/register.html'));
-})
-
-// Levantar el puerto
-app.listen(3000, () => console.log('Server corriendo en el puerto 3000'));
+app.listen(3000, () => console.log('Servidor funcionando'));
